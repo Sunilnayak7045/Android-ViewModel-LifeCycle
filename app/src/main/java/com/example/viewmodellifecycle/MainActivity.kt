eb43b61c1261d7f463f33e3138aa0d9459030512
sunilnayak7045 <sunilnayak7045@gmail.com>
2022-03-21 17:46:00 +0530

    with view model

--- app/src/main/java/com/example/viewmodellifecycle/MainActivity.kt
+++ app/src/main/java/com/example/viewmodellifecycle/MainActivity.kt
@@ -4,27 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
-    var count: Int = 0
     lateinit var txtcounter: TextView
+    lateinit var mainviewmodel: mainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainviewmodel= ViewModelProvider(this).get(mainViewModel::class.java)
 
         txtcounter = findViewById(R.id.textView_counter)
         setText()
     }
+
+    fun setText() {
+        txtcounter.text=mainviewmodel.count.toString()
+    }
+
     fun increment(v: View) {
-        count++
+        mainviewmodel.increment()
         setText()
 
     }
-
-    fun setText() {
-        txtcounter.text=count.toString()
-    }
-
 }
